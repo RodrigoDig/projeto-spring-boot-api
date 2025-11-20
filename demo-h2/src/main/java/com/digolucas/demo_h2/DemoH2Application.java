5d81097b93a189af921405f83501509b53d5e386
RodrigoDig <rodrigogomesgoncalves5367@gmail.com>
2025-11-20 14:42:33 -0300

    Inicializando BackEnd

---
+++ demo-h2/src/main/java/com/digolucas/demo_h2/DemoH2Application.java
@@ -0,0 +1,15 @@
+package com.digolucas.demo_h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoH2Application {
+
+	public static void main(String[] args) {
+
+            System.out.println("Hello Word");
+
+	}
+
+}
