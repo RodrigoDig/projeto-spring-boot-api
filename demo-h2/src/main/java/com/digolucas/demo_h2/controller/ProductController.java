88d2a4e1f1ba82b5f44a170781733fbe5af0d13f
RodrigoDig <rodrigogomesgoncalves5367@gmail.com>
2025-11-24 23:27:49 -0300

    Liberação CORS para o front

--- demo-h2/src/main/java/com/digolucas/demo_h2/controller/ProductController.java
+++ demo-h2/src/main/java/com/digolucas/demo_h2/controller/ProductController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/product")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
 
     private final ProductRepository productRepository;
